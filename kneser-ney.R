5628a23bfed55aba5e212b12b6644ed94b6d644f
Joslyn <212359551@logon.ds.ge.com>
2016-09-26 16:25:44 -0400

    two and three gram testing

--- kneser-ney.R
+++ kneser-ney.R
@@ -1,3 +1,6 @@
+if (getwd() == "C:/Users/212359551/Documents")
+  setwd("C:/Users/212359551/datascience/datasciencecapstone")
+
 # ONE_GRAM
 onegram <- read.csv("onegram.csv") 
 
@@ -11,13 +14,6 @@ Ntable$r <- as.numeric(levels(Ntable$r))[Ntable$r]
 Ntable$Nr <- as.numeric(Ntable$Nr)
 
 totalN = sum(onegram$cnt)
-
-n1 <- Ntable$Nr[Ntable$r == 1]
-n2 <- Ntable$Nr[Ntable$r == 2]
-n3 <- Ntable$Nr[Ntable$r == 3]
-n4 <- Ntable$Nr[Ntable$r == 4]
-
-d1 <- n1 / (n1 + n2)
 
 # Get Kneser-Ney probabilities for One-grams
 for(n in 1:maxR) {
@@ -33,16 +29,18 @@ Ntable$r <- as.numeric(levels(Ntable$r))[Ntable$r]
 Ntable$Nr <- as.numeric(Ntable$Nr)
 
 totalN = sum(twogram$cnt)
-maxR = length(onegram$cnt)
+maxR = length(twogram$cnt)
 
+# Get the relevant counts at 1 and 2 occurrances
 n1 <- Ntable$Nr[Ntable$r == 1]
 n2 <- Ntable$Nr[Ntable$r == 2]
-n3 <- Ntable$Nr[Ntable$r == 3]
-n4 <- Ntable$Nr[Ntable$r == 4]
 
-d1 <- n1 / (n1 + 2 * n2)
+# Calculate modified Kneser-Ney discount
+d <- n1 / (n1 + 2 * n2)
+d1 <- 1 - 2 * (d * n2 / n1)
 
 twogram$preface = as.character(twogram$preface)
+twogram$word = as.character(twogram$word)
 
 # Get Kneser-Ney probabilities for two-grams
 for(n in 1:maxR) {
@@ -50,9 +48,11 @@ for(n in 1:maxR) {
   if (twogram$onecnt[n] == 0)
     twogram$onecnt[n] = 3
   
+  twogram$complete[n] <- length(twogram$cnt[twogram$preface == twogram$preface[n]])
+  
   twogram$a[n] <- max(twogram$cnt[n] - d1,0) / twogram$onecnt[n]
-  twogram$b[n] <- d1/twogram$onecnt[n] * twogram$onecnt[n]
-  twogram$c[n] <- twogram$onecnt[n] * onegram$pkn[onegram$word == twogram$preface[n]] / totalN
+  twogram$b[n] <- d1/twogram$onecnt[n] * twogram$complete[n]
+  twogram$c[n] <- onegram$pkn[onegram$word == twogram$word[n]]
   
   twogram$pkn[n] <- twogram$a[n] + twogram$b[n] * twogram$c[n]
 }
@@ -73,19 +73,30 @@ n2 <- Ntable$Nr[Ntable$r == 2]
 n3 <- Ntable$Nr[Ntable$r == 3]
 n4 <- Ntable$Nr[Ntable$r == 4]
 
-d2 <- n2 / (n2 + 3 * n3)
+# Calculate modified Kneser-Ney discount
+d <- n1 / (n1 + 2 * n2)
+d2 <- d  # Need to adjust this
 
 threegram$preface = as.character(threegram$preface)
+threegram$X3.gram = as.character(threegram$X3.gram)
 
 # Get Kneser-Ney probabilities for two-grams
 for(n in 1:maxR) {
-  threegram$onecnt[n] <- twogram$cnt[twogram$X2.gram == threegram$preface[n]]
+  threegram$twocnt[n] <- twogram$cnt[twogram$X2.gram == threegram$preface[n]]
   if (threegram$twocnt[n] == 0)
     threegram$twocnt[n] = 1
   
+  threegram$complete[n] <- length(threegram$cnt[threegram$preface == threegram$preface[n]])
+  
+  # take the first word off the preface and add the word.
+  s <- unlist(strsplit(threegram$X3.gram[n], ' '))
+  threegram$s[n] <- paste (s[2], s[3], sep = " ")
+  
   threegram$a[n] <- max(threegram$cnt[n] - d2,0) / threegram$twocnt[n]
-  threegram$b[n] <- d2/threegram$twocnt[n] * threegram$twocnt[n]
-  threegram$c[n] <- threegram$twocnt[n] * twogram$pkn[twogram$X2.gram == threegram$preface[n]] / totalN
+  threegram$b[n] <- d2/threegram$twocnt[n] * threegram$complete[n]
+  threegram$c[n] <- twogram$pkn[twogram$X2.gram == threegram$s[n]]
   
   threegram$pkn[n] <- threegram$a[n] + threegram$b[n] * threegram$c[n]
 }
+
+threegram[threegram$preface == "is the",c("word", "X3.gram", "pkn")]
